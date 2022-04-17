76f58ad6a54504bb9ed3dfc3dfb9cc558e70c59c
NeoV0id <neov0id.nova@gmail.com>
2022-04-17 12:35:52 -0700

    Fix file 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,9 @@
 # Will modify configuration file
 
-File['ssh_config'] {
-  mode => '0600'
-}
-
 file_line { 'ssh_config':
   ensure   => 'present',
   path     => '/etc/ssh/ssh_config',
-  match    => 'IdentityFile ~/.ssh/\w',
+  match    => 'IdentityFile ~/.ssh/(\w)',
   line     => 'IdentityFile ~/.ssh/school',
   multiple => 'true'
   user     => 'root'
