70509f29789d2b4ca35cd7bbd5ac677b115377ff
NeoV0id <neov0id.nova@gmail.com>
2022-04-17 08:55:20 -0700

    Modified task 4 (file 100)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,5 @@
 # Will modify configuration file
 
-file { '/etc/ssh/ssh_config':
-  path    => '/etc/ssh/ssh_config',
-  ensure  => 'exist',
-  alias   => 'ssh_config'
-}
-
 File['ssh_config'] {
   mode => '0600'
 }
@@ -14,13 +8,12 @@ file_line { 'ssh_config':
   ensure => present,
   path   => '/etc/ssh/ssh_config',
   line   => '.ssh/school',
-  match  => 'IdentityFile +$',
+  match  => '.ssh/[.+]'
 }
 
 file_line { 'ssh_config':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
   line    => 'PasswordAuthentication no',
-  match   => 'PasswordAuthentication yes',
-  command => ['sudo systemctl restart sshd']
+  match   => 'PasswordAuthentication yes'
 }
